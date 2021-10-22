de19ce52ccd4d38674e4924f7d51ea8ae3ae3bf4
yogkri <dharmayog91@gmail.com>
2021-10-22 09:03:19 +0530

    Added: Monthly Interest Rate Calculation For Saver1 & Saver2

--- src/com/bridgelabz/BankingApplication.java
+++ src/com/bridgelabz/BankingApplication.java
@@ -3,6 +3,21 @@ package com.bridgelabz;
 public class BankingApplication {
 
     public static void main(String[] args) {
-        System.out.println("welcome to banking acct management system!");
+        // Instantiate 2 saving account objects saver1 and saver2
+        SavingsAccount saver1, saver2;
+
+        saver1 = new SavingsAccount (2000.0);
+        saver2= new SavingsAccount (3000.0);
+
+        // Set the annual interest rate to 4 % = 4/100 = 0.04
+        SavingsAccount.modifyInterestRate (0.04);
+
+        // Calculate monthly interest
+        saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
+
+        // Print new balances for both savers
+        System.out.println("This month Saver 1 balance is: "+ saver1.getSavingBalance());
+        System.out.println("This month Saver 2 balance is: "+ saver2.getSavingBalance());
     }
 }
