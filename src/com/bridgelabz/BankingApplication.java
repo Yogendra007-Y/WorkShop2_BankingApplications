64c2321a771fe2a54ffd0707b18ce40f8889fa45
yogkri <dharmayog91@gmail.com>
2021-10-22 08:16:37 +0530

    Welcome in Banking Management Program

---
+++ src/com/bridgelabz/BankingApplication.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class BankingApplication {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to banking acct management system!");
+    }
+}
